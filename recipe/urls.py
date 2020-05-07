f48e8498d546a991384e984ac94e173f73774d5d
Durgesh <durgesh123.iitr@gmail.com>
2020-05-07 21:07:00 +0530

    static cdn

--- recipe/urls.py
+++ recipe/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path('rest-auth/registration/', include('rest_auth.registration.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
