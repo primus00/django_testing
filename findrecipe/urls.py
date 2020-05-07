a73668b4569956025813dc19391167bb3ddecdbf
Durgesh <durgesh123.iitr@gmail.com>
2020-05-07 19:55:48 +0530

    feat: swagger api

--- findrecipe/urls.py
+++ findrecipe/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
     path('', views.search, name="search"),
