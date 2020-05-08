e21f9644a1c953eafa79d5ead1628a765f9a65d8
Durgesh <durgesh123.iitr@gmail.com>
2020-05-08 21:37:24 +0530

    fix: Databases

---
+++ api/tests/test_urls.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+
+class YourTestClass(TestCase):
+    def setUp(self):
+        # Setup run before every test method.
+        pass
+
+    def tearDown(self):
+        # Clean up run after every test method.
+        pass
+
+    def test_something_that_will_pass(self):
+        self.assertFalse(False)
+
+    def test_something_that_will_fail(self):
+        self.assertTrue(False)
+
+
+class YourTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        print("setUpTestData: Run once to set up non-modified data for all class methods.")
+        pass
+
+    def setUp(self):
+        print("setUp: Run once for every test method to setup clean data.")
+        pass
+
+    def test_false_is_false(self):
+        print("Method: test_false_is_false.")
+        self.assertFalse(False)
+
+    def test_false_is_true(self):
+        print("Method: test_false_is_true.")
+        self.assertTrue(True)
+
+    def test_one_plus_one_equals_two(self):
+        print("Method: test_one_plus_one_equals_two.")
+        self.assertEqual(1 + 1, 2)
+
+# DOCS: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Testing
--- api/tests/tests_models.py
+++ api/tests/tests_models.py
@@ -1,14 +1,14 @@
 from django.test import TestCase
 from api.models import User
 
-class AnimalTestCase(TestCase):
+class UserTestCase(TestCase):
     def setUp(self):
         User.objects.create(name="lion", traits="roar")
         User.objects.create(name="cat", traits="meow")
 
-    def test_animals_can_speak(self):
+    def test_adding_users(self):
         """Animals that can speak are correctly identified"""
         lion = User.objects.get(name="lion")
         cat = User.objects.get(name="cat")
-        self.assertEqual(lion.name, 'lion')
-        self.assertEqual(cat.name, 'cat')
+        self.assertEqual(lion.traits, 'roar')
+        self.assertEqual(cat.traits, 'meow')
