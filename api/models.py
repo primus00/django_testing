29aae9f46b4b46224e4716dc24643c6a445f56b3
Durgesh <durgesh123.iitr@gmail.com>
2020-05-09 00:07:34 +0530

    some testing

--- api/models.py
+++ api/models.py
@@ -9,3 +9,12 @@ class User(models.Model):
 
   def __str__(self):
     return self.name
+
+
+class User2(models.Model):
+  name = models.CharField(max_length=100)
+  description= models.TextField(max_length=5000)
+  traits = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
