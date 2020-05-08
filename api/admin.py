a37b0cf226a389689db7216331d42cd63b6314e9
Durgesh <durgesh123.iitr@gmail.com>
2020-05-09 01:30:07 +0530

    database wipe issue

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import User, User2
+from .models import User, User2, User3
 # Register your models here.
 admin.site.register(User)
 admin.site.register(User2)
+admin.site.register(User3)
--- api/models.py
+++ api/models.py
@@ -18,3 +18,11 @@ class User2(models.Model):
 
   def __str__(self):
     return self.name
+
+class User3(models.Model):
+  name = models.CharField(max_length=100)
+  description= models.TextField(max_length=5000)
+  traits = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
