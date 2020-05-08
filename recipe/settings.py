18d0b5e5054aeaad810fc0731d53698341ac15a7
Durgesh <durgesh123.iitr@gmail.com>
2020-05-09 01:25:10 +0530

    some changes

--- recipe/settings.py
+++ recipe/settings.py
@@ -147,4 +147,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/' 
 
-django_heroku.settings(locals())
+if not TESTING:
+    django_heroku.settings(locals())
