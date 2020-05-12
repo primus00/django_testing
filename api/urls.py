8ac56cd549979deeaf6a901af9bcb8ad021a3f48
Durgesh <durgesh123.iitr@gmail.com>
2020-05-13 00:14:13 +0530

    added new todo

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,18 @@
 from .router import CustomReadOnlyRouter
 from .views import UserViewSet, RandomViewSet
 
+
+"""
+    @todo Add models api.urls
+    @body There is a requirement of destroy action \
+    @body ### is a requirement of destroy action \ 
+    @body There is a requirement of destroy action \
+    @body * is a requirement of destroy action \
+    @body * is a requirement of destroy action \ 
+    @body - [ ] is a requirement of destroy action \
+"""
+
+
 router = CustomReadOnlyRouter()
 router.register('users', UserViewSet)
 router.register('try', RandomViewSet, basename='try')
