9f61c734263ce32766ee53fcf4e24e6de7862148
Durgesh <durgesh123.iitr@gmail.com>
2020-05-12 23:26:48 +0530

    Added some todo

--- api/models.py
+++ api/models.py
@@ -10,6 +10,10 @@ class User(models.Model):
   def __str__(self):
     return self.name
 
+"""
+    @todo Add models for login
+    @body There is a requirement of destroy action <br> ![img](https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.M9AsZ7Sm6Qq-LXpY92Tt2AHaEK%26pid%3DApi&f=1) 
+"""
 
 class User2(models.Model):
   name = models.CharField(max_length=100)
--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
 from .router import CustomReadOnlyRouter
 from .views import UserViewSet, RandomViewSet
 
+"""
+    @todo Add route for login api.urls
+    @body There is a requirement of destroy action <br> ![img](https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.M9AsZ7Sm6Qq-LXpY92Tt2AHaEK%26pid%3DApi&f=1) 
+"""
+
 router = CustomReadOnlyRouter()
 router.register('users', UserViewSet)
 router.register('try', RandomViewSet, basename='try')
