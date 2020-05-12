b921dcf5a89cead659ccd7a99359e0e19a7d5e3f
Durgesh <durgesh123.iitr@gmail.com>
2020-05-13 00:17:53 +0530

    more todo

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import User, User2, User3
 # Register your models here.
+
+
+"""
+    @todo Add sites api.urls
+    @body There is a requirement of destroy action <br>
+    @body ### is a requirement of destroy action <br>
+    @body There is a requirement of destroy action <br>
+    @body * is a requirement of destroy action <br>
+    @body * is a requirement of destroy action <br>
+    @body - [ ] is a requirement of destroy action <br>
+"""
+
+
 admin.site.register(User)
 admin.site.register(User2)
 admin.site.register(User3)
