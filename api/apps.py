3443859c4f1a7f8b6ae45f5e726f5aecfebd8977
Durgesh <durgesh123.iitr@gmail.com>
2020-05-12 23:26:48 +0530

    Added some todo

--- api/apps.py
+++ api/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class ApiConfig(AppConfig):
     name = 'api'
+
+"""
+    @todo Add new ApiConfig
+    @body There is a requirement of destroy action <br> ![img](https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.M9AsZ7Sm6Qq-LXpY92Tt2AHaEK%26pid%3DApi&f=1) 
+"""
